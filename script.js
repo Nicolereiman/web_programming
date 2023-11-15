de2fce08a22369df55e6f4a67e20c11a001f8e7d
Nicolereiman <101124750+Nicolereiman@users.noreply.github.com>
2023-11-14 21:03:45 -0300

    14nov

--- script.js
+++ script.js
@@ -1,122 +1,126 @@
+
+//----------RESERVAS 
+// Armo una lista de reservas
 const reservas = {
   lista: [],
   agregarReserva: function(reserva) {
+    // Agrego la nueva reserva, cargada en el formulario,  a la lista
     this.lista.push(reserva);
   },
 };
 
+// Función para guardar los datos del formulario de reservas
 function guardarFormulario() {
-  // Tomamos los valores del formulario
+  // Extraigo los valores del formulario
   const nombre = document.getElementById("nombre").value;
   const nacimiento = document.getElementById("nacimiento").value;
   const email = document.getElementById("email").value;
   const tour = document.getElementById("tour").value;
 
-  // Creamos un objeto con los datos del formulario
-  const reserva = {
-    nombre: nombre,
-    nacimiento: nacimiento,
-    email: email,
-    tour: tour,
-  };
+  // Validación:  todos los campos tienen que estar completos, si alguno no está no se proesan los datos
+  if (!nombre || !nacimiento || !email || !tour) {
+    // Alerta al usuario si faltan datos
+      Swal.fire({
+        title: "Error!",
+        text: "Por favor, completa todos los campos del formulario.!",
+        icon: "error"
+      });
+    
+    return;
+  }
 
-  // Agregamos este objeto al objeto 'reservas'
+  // Objeto literal (diccionario) con los datos del formulario
+  const reserva = { nombre, nacimiento, email, tour };
+
+  // Agrego la reserva al objeto 'reservas'
   reservas.agregarReserva(reserva);
 
-  // Muestra el mensaje de reserva
+  // Mensaje de confirmación para el usuario, para confirmar la reserva
   const mensajeReserva = `
     <p>${nombre}, tu reserva está confirmada para el tour ${tour}.</p>
     <p>Te contactaremos a tu casilla de correo ${email} para coordinar el horario según disponibilidad.</p>
   `;
 
+  // Muestro el mensaje de reserva en la página
   const mensajeReservaElement = document.getElementById("mensaje-reserva");
   mensajeReservaElement.innerHTML = mensajeReserva;
-  // Cambia el estilo para que el div sea visible
-  mensajeReservaElement.style.display = "block"; 
+  mensajeReservaElement.style.display = "block";
+
+  //reiniciar el formulario cuando ya se procesó, por si se quiere hacer una nueva reserva
+  document.getElementById("formularioReserva").reset();
 }
 
-// hora actual
-var horaActual = new Date().getHours();
-var body = document.body;
-
-// Función para cambiar el fondo según el momento del día
-function cambiarFondoSegunHora() {
-    if (horaActual >= 6 && horaActual < 16) {
-        // Mañana (de 6:00 a 11:59)
-        body.style.backgroundImage = "url('fondo4.jpg')";
-    } else if (horaActual >= 16 && horaActual < 21) {
-        // Tarde (de 16:00 a 20:59)
-        body.style.backgroundImage = "url('fondo6.jpg')";
-    } else {
-        // Noche (de 21:00 a 05:59)
-        body.style.backgroundImage = "url('fondo3.jpg')";
-    }
-}
-
-// Llama a la función para cambiar el fondo
-cambiarFondoSegunHora();
-
-
-    /*
-  
-
-    // Definimos una lista de reseñas
-const listaReseñas = [];
-// Función para agregar una reseña a la lista
-function mostrarAgradecimiento(datos) {
-  function mostrarAgradecimiento(datos) {
-    const mensajeAgradecimiento = `
-      <div class="agradecimiento">
-        <p>¡Gracias ${datos.nombre} por compartir tu experiencia!</p>
-        <p>Estos son los datos ingresados:</p>
-        <ul>
-          <li>Nombre: ${datos.nombre}</li>
-          <li>Edad: ${datos.edad}</li>
-          <li>Ciudad: ${datos.ciudad}</li>
-          <li>Puntuación: ${datos.puntuacion}</li>
-          <li>Comentario: ${datos.comentario}</li>
-        </ul>
-        <p>Revisaremos que nada infrinja nuestras normas de convivencia y la subiremos al sitio.</p>
-        <p>¡Esperamos verte pronto!</p>
-      </div>
-    `;
-  
-    const agradecimientoElement = document.getElementById("agradecimiento");
-    agradecimientoElement.innerHTML = mensajeAgradecimiento;
-  }
-  
-
+//----------RESEÑAS 
 function guardarResena() {
-  // Obtenemos los valores del formulario de reseñas
+  // Obtengo los valores del formulario de reseñas
   const nombre = document.getElementById("nombreResena").value;
   const edad = document.getElementById("edadResena").value;
   const ciudad = document.getElementById("ciudadResena").value;
-  const puntuacion = document.getElementById("puntuacionResena").value;
   const comentario = document.getElementById("comentarioResena").value;
+  const puntuacion = document.getElementById("puntuacionResena").value;
 
-  // Creamos un objeto literal con la información
-  const reseña = {
+  // Validación de la reseña
+  if (!nombre || !edad || !ciudad || !comentario || !puntuacion) {
+    // Muestra un mensaje de error si faltan datos
+    Swal.fire({
+      title: "Error!",
+      text: "Por favor, completa todos los campos del formulario.!",
+      icon: "error"
+    });
+    return;
+  }
+
+  // Validación de la edad (por ejemplo, debe ser un número positivo)
+  if (isNaN(edad) || edad <= 0) {
+    // Muestra un mensaje de error si la edad no es válida
+    Swal.fire({
+      title: "Error!",
+      text: "Por favor, ingresá una edad válida!",
+      icon: "error"
+    });
+    return;
+  }
+
+  // Creo un objeto con la información de la nueva reseña
+  const nuevaResena = {
     nombre: nombre,
     edad: edad,
     ciudad: ciudad,
-    puntuacion: puntuacion,
     comentario: comentario,
+    puntuacion: puntuacion,
   };
-   // Llama a la función para mostrar las reseñas actualizadas
-  mostrarAgradecimiento(reseña);
-  // Agregamos la reseña a la lista
-  listaReseñas.push(reseña);
 
-  // Limpia los campos del formulario de reseñas
-  document.getElementById("nombreResena").value = "";
-  document.getElementById("edadResena").value = "";
-  document.getElementById("ciudadResena").value = "";
-  document.getElementById("puntuacionResena").value = "5"; // Restaurar la puntuación predeterminada
-  document.getElementById("comentarioResena").value = "";
+  // Muestra la notificación de agradecimiento
+    Swal.fire({
+      title: "Todo listo!",
+      text: " Tu reseña ha sido recibida. Revisaremos que cumpla con nuestras normas de convivencia antes de publicarla.",
+      icon: "success"
+    });
 
- 
+  // Reinicio el formulario
+  document.getElementById("formularioResena").reset();
 }
-}Este es un comentario multilínea en JavaScript.
-  Puedes escribir varias líneas de comentarios aquí.
-  Esto es útil para explicar bloques de código extensos.
-*/
+
+
+
+
+
+
+//Esta idea nunca funcionó
+function cambiarFondoSegunHora() {
+  console.log("Cambiando fondo según la hora...");
+  if (horaActual >= 6 && horaActual < 16) {
+    console.log("Es de día (mañana)");
+    body.style.backgroundImage = "url('fondo4.jpg')";
+  } else if (horaActual >= 16 && horaActual < 21) {
+    console.log("Es de tarde");
+    body.style.backgroundImage = "url('fondo6.jpg')";
+  } else {
+    console.log("Es de noche");
+    body.style.backgroundImage = "url('fondo3.jpg')";
+  }
+}
+
+
+// llamar a la función 
+cambiarFondoSegunHora()
